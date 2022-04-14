83f0582df1c3be2eb3f0d0f733e187c59b873d19
ningerdinger <ningweizhou1991@gmail.com>
2022-04-14 21:37:38 +0200

    Connecting with API

--- Connector.py
+++ Connector.py
@@ -4,9 +4,32 @@ The source is from
 https://eodhistoricaldata.com/financial-apis/?roistat=google1_g_128816179848_534670027147_stock%20api&roistat_referrer=&roistat_pos=&utm_source=google_search&utm_medium=cpc&utm_campaign=EXT_Europe&roistat_referrer=&roistat_pos=&roistat=google1_g_128816179848_534670027147_stock%20api&gclid=Cj0KCQjwjN-SBhCkARIsACsrBz493ul8kflB6ZqeaFZxXAiZevSw09KWVnj2Ly-6ZiOvu-FQ_1ypFPIaAgPkEALw_wcB
 '''
 
+import os
+from eod import EodHistoricalData
+from random import randint
+
+
+
+
 class Connector:
 
-    def __init__(self):
-        self.test = "Hello world"
-        print(self.test)
-        print("fack me")
+    def __init__(self, url):
+        self.url = url
+        # load the key from the enviroment variables
+        api_key = os.environ['API_EOD']
+
+        # Create the instance 
+        client = EodHistoricalData(api_key)
+        # predefine some instruments
+        symbol='AAPL.US'
+        goverment_bond = 'SW10Y.GBOND'
+        corporate_bond = 'US00213MAS35.BOND'
+
+        # Quick usage
+        # weekly prices for the Swiss goverment bond
+        stock_prices = client.get_prices_eod(goverment_bond, period='w', order='a')
+        # Short interest
+        get_short_interest = client.get_short_interest(symbol, to='2021-07-04')
+        # Fundamental data for the stock
+        resp = client.get_fundamental_equity(symbol, filter_='Financials::Balance_Sheet::quarterly') # Stock - check
+
---
+++ advanced_test.py
@@ -0,0 +1,21 @@
+import requests
+import os
+import pandas as pd
+
+from io import StringIO
+
+def get_eod_data(symbol="AAPL.US", api_token=os.environ['API_EOD'], session=None):
+    if session is None:
+        session = requests.Session()
+        url = 'https://eodhistoricaldata.com/api/eod/%s' # symbol
+
+        params = {"api_token": api_token}
+
+        r = session.get(url, params=params)
+
+        if r.status_code == requests.codes.ok:
+            df = pd.read_csv(StringIO(r.text), skipfooter=1, parse_dates=[0], index_col=0, engine='python')
+            return df
+            
+        else:
+            raise Exception(r.status_code, r.reason, url)
