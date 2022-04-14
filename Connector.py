66889bea6a3702d90823c1b9fd46a1cfed96f572
ningerdinger <ningweizhou1991@gmail.com>
2022-04-14 21:07:42 +0200

    Requirements for pip

--- Connector.py
+++ Connector.py
@@ -1,3 +1,9 @@
+'''
+The source is from
+
+https://eodhistoricaldata.com/financial-apis/?roistat=google1_g_128816179848_534670027147_stock%20api&roistat_referrer=&roistat_pos=&utm_source=google_search&utm_medium=cpc&utm_campaign=EXT_Europe&roistat_referrer=&roistat_pos=&roistat=google1_g_128816179848_534670027147_stock%20api&gclid=Cj0KCQjwjN-SBhCkARIsACsrBz493ul8kflB6ZqeaFZxXAiZevSw09KWVnj2Ly-6ZiOvu-FQ_1ypFPIaAgPkEALw_wcB
+'''
+
 class Connector:
 
     def __init__(self):
